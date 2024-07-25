cb0441fb9c10c91b8d1d8e7e1ddaa76436396283
Nikolay Kirilyk <fomabb@yandex.by>
2024-07-25 13:57:41 +0300

    init

---
+++ travel-diary-youtube/src/main/java/com/iase24/travel_diary_youtube/TravelDiaryYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.iase24.travel_diary_youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDiaryYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDiaryYoutubeApplication.class, args);
+	}
+
+}
